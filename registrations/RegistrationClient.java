6ba3b86c7c4c5d0f082cc27dceadc7ceba2f2aed
homeuser <homeuser@homenet.(none)>
2011-10-13 13:21:16 +0100

    RegistrationClient now asks for router ip before connecting

--- registrations/RegistrationClient.java
+++ registrations/RegistrationClient.java
@@ -6,12 +6,13 @@ import java.net.Socket;
 public class RegistrationClient{
 	public static void main(String[] args){
 		try{
-			InetAddress addr = InetAddress.getByName("10.2.0.2");
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter router address");			
+       			InetAddress addr = InetAddress.getByName(stdIn.readLine());
 			int port = 5678;
 			Socket socket = new Socket(addr, port);
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 			String fromServer, fromUser;
 			while ((fromServer = in.readLine()) != null){
 				System.out.println(fromServer);
